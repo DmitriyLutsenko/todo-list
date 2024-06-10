f79b92340cbe46cc0ddd4b79ac30853ad6d47262
Dmitriy Lutsenko <dmitriy.lutsenko.95@mail.ru>
2024-06-10 23:48:26 +0400

    Начало работ по лейблам

---
+++ database/migrations/2024_06_10_233014_create_labels_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100)->unique();
+            $table->string('bcolor', 7)->default('#89e690');
+            $table->string('tcolor', 7)->default('#ffffff');
+            $table->timestamps();
+        });
+
+        Schema::create('task_label', function (Blueprint $table) {
+            $table->unsignedBigInteger('task_id')->unique();
+            $table->unsignedBigInteger('label_id');
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('label_id')->references('id')->on('labels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_label');
+        Schema::dropIfExists('labels');
+    }
+};
